7ca924f2e4dc9094d0d0e77990bc542fbfa0af5e
Dami <damion@spiti.co.za>
2025-07-29 22:27:49 +0200

    Refactor IL training and update experiment data
    Refactored imitation learning training loop in imitation.py to support loading a model from a specified path and adjusted save frequency. Updated predicted_tree fields in mixed_static_pixels_big episode data for improved tree structure accuracy. Modified get_tree_metrics.ipynb and find_episode_data.ipynb for dynamic task selection and improved error handling. Added new experiment logs and updated flagfiles and episode data for multiple experiment runs.

--- imitation.py
+++ imitation.py
@@ -42,8 +42,9 @@ FLAGS = flags.FLAGS
 flags.DEFINE_bool('il_demo_from_model', default=False, help='Whether to use demo from bot')
 flags.DEFINE_integer('il_train_steps', default=50, help='Trainig steps')
 flags.DEFINE_integer('il_eval_freq', default=20, help='Evaluation frequency')
-flags.DEFINE_integer('il_save_freq', default=200, help='Save freq')
+flags.DEFINE_integer('il_save_freq', default=50, help='Save freq')
 flags.DEFINE_bool('il_no_done', default=False, help='Whether or not to use done during IL')
+flags.DEFINE_string('model_path', default='', help='Path of the model used if il_demo_from_model is True')
 
 # Data
 flags.DEFINE_float('il_val_ratio', default=0.2,
@@ -202,75 +203,70 @@ def main_loop(bot, dataloader, opt, training_folder, test_dataloader=None):
     nb_frames = 0
     train_stats = DictList()
     curr_best = 0
-    while True:
-        # print('Training steps: {}'.format(train_steps))
-        if train_steps > FLAGS.il_train_steps:
-            print('Reaching maximum steps')
-            break
 
-        if train_steps % FLAGS.il_save_freq == 0:
-            with open(os.path.join(training_folder, 'bot{}.pkl'.format(train_steps)), 'wb') as f:
-                torch.save(bot, f)
 
-        # if train_steps % FLAGS.il_eval_freq == 0:
-        #     print('Evaluating...')
-        #     # testing on valid
-        #     val_metrics = evaluate_on_envs(bot, dataloader)
-        #     logging_metric(nb_frames, train_steps, val_metrics, writer, prefix='val')
-
-        #     # testing on test env
-        #     if test_dataloader is not None:
-        #         test_metrics = evaluate_on_envs(bot, test_dataloader)
-        #         logging_metric(nb_frames, train_steps, test_metrics, writer, prefix='test')
-
-        #     avg_ret = [val_metrics[env_name]['ret'].item() for env_name in val_metrics]
-        #     avg_ret = np.mean(avg_ret)
-
-        #     if avg_ret > curr_best:
-        #         curr_best = avg_ret
-        #         print('Save Best with return: {}'.format(avg_ret))
-
-        #         # Save the checkpoint
-        #         with open(os.path.join(training_folder, 'bot_best.pkl'), 'wb') as f:
-        #             torch.save(bot, f)
-
-        # Forward/Backward
-        bot.train()
-        train_batch, train_lengths = train_iter.__next__()
+    if FLAGS.model_path != '':
+        print(f"Loading model from {FLAGS.model_path}")
+        with open(FLAGS.model_path, 'rb') as f:
+            bot = torch.load(f)
         if FLAGS.cuda:
-            train_batch.apply(lambda _t: _t.cuda())
-            train_lengths = train_lengths.cuda()
+            bot = bot.cuda()
+        print('Using demo from model: {}'.format(bot))
+    else:
+        print('Training Model')
+        while True:
+            # print('Training steps: {}'.format(train_steps))
+            if train_steps > FLAGS.il_train_steps:
+                print('Reaching maximum steps')
+                break
 
-        start = time.time()
-        train_batch_res = run_batch(train_batch, train_lengths, bot)
-        train_batch_res.apply(lambda _t: _t.sum() / train_lengths.sum())
-        batch_time = time.time() - start
-        loss = train_batch_res.ce_loss
-        opt.zero_grad()
-        loss.backward()
-        params = [p for p in bot.parameters() if p.requires_grad]
-        grad_norm = torch.nn.utils.clip_grad_norm_(parameters=params, max_norm=FLAGS.il_clip)
-        opt.step()
-        train_steps += 1
-        nb_frames += train_lengths.sum().item()
-        fps = train_lengths.sum().item() / batch_time
+            if train_steps % FLAGS.il_save_freq == 0:
+                with open(os.path.join(training_folder, 'bot{}.pkl'.format(train_steps)), 'wb') as f:
+                    torch.save(bot, f)
 
-        stats = DictList()
-        stats.grad_norm = grad_norm
-        stats.acc = train_batch_res.acc.detach()
-        stats.ce_loss = train_batch_res.ce_loss.detach()
-        stats.fps = torch.tensor(fps)
-        train_stats.append(stats)
+            # Forward/Backward
+            bot.train()
+            train_batch, train_lengths = train_iter.__next__()
+            if FLAGS.cuda:
+                train_batch.apply(lambda _t: _t.cuda())
+                train_lengths = train_lengths.cuda()
 
-        if train_steps % FLAGS.il_eval_freq == 0:
-            train_stats.apply(lambda _tensors: torch.stack(_tensors).mean().item())
-            logger_str = ['[TRAIN] steps={}'.format(train_steps)]
-            for k, v in train_stats.items():
-                logger_str.append("{}: {:.4f}".format(k, v))
-                writer.add_scalar('train/' + k, v, global_step=nb_frames)
-            print('\t'.join(logger_str))
-            train_stats = DictList()
-            writer.flush()
+            start = time.time()
+            train_batch_res = run_batch(train_batch, train_lengths, bot)
+            train_batch_res.apply(lambda _t: _t.sum() / train_lengths.sum())
+            batch_time = time.time() - start
+            loss = train_batch_res.ce_loss
+            opt.zero_grad()
+            loss.backward()
+            params = [p for p in bot.parameters() if p.requires_grad]
+            grad_norm = torch.nn.utils.clip_grad_norm_(parameters=params, max_norm=FLAGS.il_clip)
+            opt.step()
+            train_steps += 1
+            nb_frames += train_lengths.sum().item()
+            fps = train_lengths.sum().item() / batch_time
+
+            stats = DictList()
+            stats.grad_norm = grad_norm
+            stats.acc = train_batch_res.acc.detach()
+            stats.ce_loss = train_batch_res.ce_loss.detach()
+            stats.fps = torch.tensor(fps)
+            train_stats.append(stats)
+
+            if train_steps % FLAGS.il_eval_freq == 0:
+                train_stats.apply(lambda _tensors: torch.stack(_tensors).mean().item())
+                logger_str = ['[TRAIN] steps={}'.format(train_steps)]
+                for k, v in train_stats.items():
+                    logger_str.append("{}: {:.4f}".format(k, v))
+                    writer.add_scalar('train/' + k, v, global_step=nb_frames)
+                print('\t'.join(logger_str))
+                train_stats = DictList()
+                writer.flush()
+
+            
+        # Ensure model is in eval mode for final save
+        bot.eval()
+        with open(os.path.join(training_folder, 'bot_final.pkl'), 'wb') as f:
+            torch.save(bot, f)
 
     # === APPENDED: extract and print skill IDs per episode ===
     print("\nExtracting skill assignments across validation episodes...")
@@ -399,11 +395,11 @@ def main_loop(bot, dataloader, opt, training_folder, test_dataloader=None):
             depths = np.digitize(depths.cpu().numpy(), np.linspace(0, 1, 5))
             
             # Convert actions to vocabulary strings
-            action_strs = [idx for idx in actions_cpu.squeeze()]
+            action_strs = [str(idx) for idx in actions_cpu.squeeze()]
             
             # Use distance2ctree with depth information
-            # pred_tree = distance2ctree(depths, action_strs, binary=False)
-            pred_tree = predicted_data_to_hierarchical_tree(_action, predicted)
+            pred_tree = distance2ctree(depths, action_strs, binary=False)
+            # pred_tree = predicted_data_to_hierarchical_tree(_action, predicted)
             tree_str = tree_to_str(pred_tree)
             # print(f"{'Predicted Tree:':20} {tree_str}")
 
@@ -568,6 +564,7 @@ def run(training_folder):
     except KeyboardInterrupt:
         pass
 
-    # Save the checkpoint
+    # Save the checkpoint - ensure consistent state with training
+    bot.eval()
     with open(os.path.join(training_folder, 'bot.pkl'), 'wb') as f:
         torch.save(bot, f)
